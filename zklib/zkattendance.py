7c89d6f66217f8f1efb04faa26868c7f629a841f
franzemil35 <franzemil35@gmail.com>
2017-06-06 10:10:09 -0400

    Cambio del ancho del buffer

--- zklib/zkattendance.py
+++ zklib/zkattendance.py
@@ -34,9 +34,6 @@ def reverseHex(hexstr):
     
 def zkgetattendance(self):
     """Start a connection with the time clock"""
-    
-
-
     command = CMD_ATTLOG_RRQ
     command_string = ''
     chksum = 0
@@ -57,10 +54,10 @@ def zkgetattendance(self):
             bytes = getSizeAttendance(self)
            #bytes = bytes + 10
             while bytes > 0:
-                data_recv, addr = self.zkclient.recvfrom(1032)
+                data_recv, addr = self.zkclient.recvfrom(2048)
                 #pp.pprint(data_recv)
                 self.attendancedata.append(data_recv)
-                bytes -= 1024#1024
+                bytes -= 2048#1024
                 
             self.session_id = unpack('HHHH', self.data_recv[:8])[2]
             data_recv = self.zkclient.recvfrom(8)
